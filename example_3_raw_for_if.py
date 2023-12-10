29c5018baa12f1f776d7299e14114ce77e891ca5
kraft <krafstov@gmail.com>
2023-12-10 15:51:44 +0300

    ex_3 - add file

---
+++ example_3_raw_for_if.py
@@ -0,0 +1,17 @@
+from jinja2 import Template
+
+name = "Вася"
+
+link = '''В HTML-документе ссылки определяются так: 
+<a href="#">Ссылка</a>'''
+
+# конструкция {% raw %} ... {% endraw %}
+templ = Template(
+    "Пример экранирования имени {{ name }}, в этом случае {% raw %} {{ name }} не экранируется {% endraw %}!"
+)
+
+templ_link = Template("Ссылка будет экранирована и будут выведены символы:\n{{link | e}}")
+
+print(templ.render(name=name))
+print()
+print(templ_link.render(link=link))
