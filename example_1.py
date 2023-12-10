a99b735f85af6e8c004769a605e365cd54cc2537
kraft <krafstov@gmail.com>
2023-12-10 12:53:46 +0300

    ex_1 - added dict, ex_2 - renamed

--- example_1.py
+++ example_1.py
@@ -1,10 +1,26 @@
 from jinja2 import Template
 
+# переменные:
 name = "Вася"
 age = 25
 
+# словарь:
+person = {
+    "name": name,
+    "age": age
+}
+
+# выдергиваем переменные напрямую:
 info_about_man = Template("Зовут меня {{ name }}, мне {{ age }} лет.")
 
 msg = info_about_man.render(name=name, age=age)
 
+
+# выдергиваем переменные через словарь обращаясь по ключу:
+info_from_dict = Template("Зовут меня {{ person.name }}, мне {{ person.age }} лет.")
+
+msg2 = info_from_dict.render(person=person)
+
 print(msg)
+print()
+print(msg2)
